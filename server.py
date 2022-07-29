6d1ca18d0dbe166871bbe639e65180f94721e906
Jacques Arnoux <jacques@dioptra.ai>
2022-07-29 12:05:19 -0700

    return response

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/ingest', methods = ['POST'])
 def ingest():
     print('Ingestion service received a request')
-    return 'ingested'
+    return f'Ingested {len(request.json["records"])} records'
