eb89899157cfb7e22905034ef795497ca3e66066
Jacques Arnoux <jacques@dioptra.ai>
2022-07-29 20:17:14 -0700

    basic insert statement

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM python:3.9.7-slim
+FROM python:3.9-slim
 MAINTAINER "Jacques Arnoux <jacques@dioptra.ai>"
 EXPOSE 8082
 
 WORKDIR /app/
+RUN apt-get update && \
+    apt-get install -y build-essential python3.9-dev libpq-dev
 COPY ./requirements.txt ./requirements.txt
 RUN pip3 install -r requirements.txt
 COPY . .
--- server.py
+++ server.py
@@ -1,8 +1,15 @@
 from flask import Flask, request
+from postgres import connection
 
 app = Flask(__name__)
 
 @app.route('/ingest', methods = ['POST'])
 def ingest():
-    print('Ingestion service received a request')
+    records = request.json['records']
+    print(f'records: {records}')
+
+    cur = connection.cursor()
+    for record in records:
+        cur.execute(f"INSERT INTO events ({','.join(record.keys())}) VALUES ({','.join(['%s'] * len(record))})", list(record.values()))
+
     return f'Ingested {len(request.json["records"])} records'
