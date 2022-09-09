0ea6b4b6f6727c57ff451a50ef73ebe83b7451bf
Jacques Arnoux <jacques@dioptra.ai>
2022-09-08 18:40:27 -0700

    fixed unflattening of fields

--- helpers/compatibility.py
+++ helpers/compatibility.py
@@ -12,18 +12,19 @@ def process(event):
     return event
 
 def unflatten_path(path, value):
-    if len(path) == 1:
-        return {
-            path[0]: value
-        };
-    else:
-        return {
-            path[0]: unflatten_path(path[1:], value)
-        };
+
+    return {
+        path[0]: value if len(path) == 1 else unflatten_path(path[1:], value)
+    };
 
 def unflatten_dict(prefix, my_dict):
     key_values = list(my_dict.items())
     for key, value in key_values:
         if key.startswith(f'{prefix}.'):
-            my_dict[prefix] = unflatten_path(key.split('.')[1:], value)
+
+            if prefix not in my_dict:
+                my_dict[prefix] = {}
+
+            my_dict[prefix].update(unflatten_path(key.split('.')[1:], value))
+
             del my_dict[key]
