9a97273f6eeadcf596b814811d36d30f5d799790
Jacques Arnoux <jacques@dioptra.ai>
2022-09-15 16:21:11 -0700

    now using threadpool for batc ingestion and added prints

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
+from concurrent.futures import ThreadPoolExecutor
 import itertools
 import logging
 import os
-from threading import Thread
+from threading import Thread, current_thread
 import time
 import werkzeug
 from multiprocessing import Pool
@@ -50,7 +51,7 @@ def process_events(events, organization_id):
             events
         )
 
-        print(f'Processed {len(events)} events in {time.time() - tic} seconds')
+        print(f'[{current_thread().name}] Processed {len(events)} events in {time.time() - tic} seconds')
 
         # event_processor.process_event returns a list of events for each parent event
         return list(itertools.chain(*events))
@@ -60,13 +61,16 @@ MAX_BATCH_SIZE = 1000
 
 def flush_events(events):
     session = get_session()
+    print(f'[{current_thread().name}] Got session events...')
     try:
         session.add_all([Event(**{
             k: v for k, v in event.items() if k in valid_event_attrs
         }) for event in events])
+        print(f'[{current_thread().name}] Created event objects...')
         tic = time.time()
+        print(f'[{current_thread().name}] Flushing...')
         session.commit()
-        print(f'Flushed {len(events)} events in {time.time() - tic} seconds')
+        print(f'[{current_thread().name}] Flushed {len(events)} events in {time.time() - tic} seconds')
     except TypeError as e:
 
         raise werkzeug.exceptions.BadRequest(str(e))
@@ -89,23 +93,25 @@ def process_batches(urls, organization_id):
                     try:
                         batched_events.append(orjson.loads(dioptra_record_str))
                     except:
-                        print(f'Could not parse JSON record in {url}[{line_num}]')
+                        print(f'[{current_thread().name}] Could not parse JSON record in {url}[{line_num}]')
                     else:
                         if len(batched_events) >= MAX_BATCH_SIZE:
                             try:
                                 processed_events = process_events(batched_events, organization_id)
+                                print(f'[{current_thread().name}] Now flushing...')
                                 flush_events(processed_events)
-                            except Exception as e:
-                                print(f'Failed to process or flush events: {e}. Moving on...')
+                            except:
+                                import traceback
+                                print(f'[{current_thread().name}] Failed to process or flush events: {traceback.format_exc()}. Moving on...')
                             finally:
                                 batched_events = []
                     finally:
                         line_num += 1
 
-                print(f'Processed {i + 1} of {len(urls)} batches')
+                print(f'[{current_thread().name}] Processed {i + 1} of {len(urls)} batches')
 
             except Exception as e:
-                print(f'Failed to process {url}: {e}, moving on...')
+                print(f'[{current_thread().name}] Failed to process {url}: {e}, moving on...')
 
         if len(batched_events):
             processed_events = process_events(batched_events, organization_id)
@@ -114,6 +120,8 @@ def process_batches(urls, organization_id):
     except Exception as e:
         logging.exception(e)
         # TODO: Log this somewhere useful for the user to see ingestion failures.
+
+thread_pool = ThreadPoolExecutor()
 
 @app.route('/ingest', methods = ['POST'])
 def ingest():
@@ -128,8 +136,7 @@ def ingest():
         flush_events(events)
     elif 'urls' in body:
         print(f"Received {len(body['urls'])} batch urls for organization {organization_id}")
-        daemon = Thread(target=process_batches, daemon=True, args=(body['urls'], organization_id))
-        daemon.start()
+        thread_pool.submit(process_batches, body['urls'], organization_id)
     else:
         raise werkzeug.exceptions.BadRequest('No records or batch urls provided.')
 
