09f70a91ba6703a2a2a8b2cfebc5acdff49e4850
Pierre <pierre@dioptra.ai>
2023-03-26 19:24:15 -0400

    Adding mcdo sem seg unit test

--- helpers/predictions_test.py
+++ helpers/predictions_test.py
@@ -74,3 +74,38 @@ def test_sem_seg_process_predictions():
     assert prediction.metrics['entropy'] >= 0
     assert prediction.metrics['entropy'] <= 1
     assert len(prediction.confidences) == 19
+    assert session.execute.call_count == 2
+
+def test_mcdo_sem_seg_process_predictions():
+    session = UnifiedAlchemyMagicMock()
+
+    max_mask_size = 10
+    os.environ['MAX_MASK_SIZE'] = str(max_mask_size)
+
+    with ZipFile(os.path.join(TEST_DATA_DIR, 'mcdo_semseg_pred_payload.json.zip')) as myzip:
+        with myzip.open('mcdo_semseg_pred_payload.json') as file:
+            record = json.load(file)
+    record['organization_id'] = '123'
+    record['predictions'] = [record['prediction']]
+
+    datapoint = Datapoint
+    session.add(datapoint)
+
+    process_prediction_records(record['predictions'], datapoint, session)
+
+    results = session.query(Prediction).all()
+    assert len(results) == 1
+    prediction = results[0]
+    assert prediction.datapoint == datapoint.id
+    assert prediction.task_type == 'SEGMENTATION'
+
+    assert len(compute_shape(decode_to_np_array(prediction.encoded_segmentation_class_mask))) == 2
+    assert compute_shape(decode_list(prediction.encoded_resized_segmentation_class_mask)) == (max_mask_size, max_mask_size)
+
+    assert prediction.metrics['entropy'] >= 0
+    assert prediction.metrics['entropy'] <= 1
+    assert prediction.metrics['variance'] >= 0
+    assert prediction.metrics['variance'] <= 1
+    assert len(prediction.confidences) == 19
+    assert session.execute.call_count == 3
+
