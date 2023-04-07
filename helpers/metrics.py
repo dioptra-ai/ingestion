6af7261f237ea25bbefa48c0b57a2e293ea2aafb
Jacques Arnoux <jacques@dioptra.ai>
2023-04-06 18:02:57 -0700

    supporting class_names shorter than actual number of classes in masks

--- helpers/metrics.py
+++ helpers/metrics.py
@@ -5,8 +5,9 @@ def segmentation_distribution(segmentation_class_mask, class_names = None):
     values = values.astype(int).tolist()
     counts = counts.astype(int).tolist()
     class_names = class_names or [str(v) for v in range(max(values) + 1)]
+    num_classes = len(class_names)
     distribution = {}
     for value, count in zip(values, counts):
-        distribution[class_names[value]] = count
+        distribution[class_names[value] if value < num_classes else str(value)] = count
     
     return distribution
