a0ef8c298cec3ee05aa497370f0e7d018e97dfc8
Jacques Arnoux <jacques@dioptra.ai>
2023-04-19 14:41:22 -0700

    polygon and seg masks generate bbox top left width height

--- helpers/bboxes.py
+++ helpers/bboxes.py
@@ -22,6 +22,15 @@ def process_bbox_records(bboxes, pg_session, prediction=None, groundtruth=None):
             )
             pg_session.add(bbox)
         
+        # coco_polygon is a flat list of (x, y, x, y, ...) coordinates.
+        if 'coco_polygon' in b:
+            bbox.coco_polygon = b['coco_polygon']
+            x_values = bbox.coco_polygon[::2]
+            y_values = bbox.coco_polygon[1::2]
+            bbox.top = b.get('top', min(y_values))
+            bbox.left = b.get('left', min(x_values))
+            bbox.height = b.get('height', max(y_values) - min(y_values))
+            bbox.width = b.get('width', max(x_values) - min(x_values))
         if 'top' in b:
             bbox.top = b['top']
         if 'left' in b:
@@ -30,15 +39,21 @@ def process_bbox_records(bboxes, pg_session, prediction=None, groundtruth=None):
             bbox.height = b['height']
         if 'width' in b:
             bbox.width = b['width']
-        if 'coco_polygon' in b:
-            bbox.coco_polygon = b['coco_polygon']
 
         if 'segmentation_mask' in b:
             segmentation_mask = b['segmentation_mask']
             if segmentation_mask and np.array(segmentation_mask).size > 0:
+                segmentation_mask = np.array(segmentation_mask).astype(np.uint8)
                 bbox.encoded_segmentation_mask = encode_np_array(b['segmentation_mask'])
                 bbox.encoded_resized_segmentation_mask = encode_list(resize_mask(b['segmentation_mask']))
-                bbox.metrics = {**bbox.metrics} if bbox.metrics else {}
+                # Top is the row index of the topmost nonzero element in the mask.
+                # Left is the column index of the leftmost nonzero element in the mask.
+                # Height is the number of nonzero rows in the mask.
+                # Width is the number of nonzero columns in the mask.
+                bbox.top = b.get('top', int(np.nonzero(segmentation_mask)[0].min()))
+                bbox.left = b.get('left', int(np.nonzero(segmentation_mask)[1].min()))
+                bbox.height = b.get('height', int(np.nonzero(segmentation_mask)[0].max() - bbox.top))
+                bbox.width = b.get('width', int(np.nonzero(segmentation_mask)[1].max() - bbox.left))
             else:
                 bbox.encoded_segmentation_mask = None
                 bbox.encoded_resized_segmentation_mask = None
