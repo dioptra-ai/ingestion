e40c4993444c3c086e714249354947a5f44a7b49
Jacques Arnoux <jacques@dioptra.ai>
2023-04-20 15:00:02 -0700

    adds confidences and metrics to lanes

--- helpers/lanes.py
+++ helpers/lanes.py
@@ -1,5 +1,7 @@
 from schemas.pgsql import models
 
+from helpers.common import process_confidences
+
 Lanes = models.lane.Lane
 
 def process_lane_records(lanes, pg_session, prediction=None, groundtruth=None):
@@ -17,6 +19,21 @@ def process_lane_records(lanes, pg_session, prediction=None, groundtruth=None):
         # coco_polyline is a flat list of (x, y, x, y, ...) coordinates.
         if 'coco_polyline' in l:
             lane.coco_polyline = l['coco_polyline']
+
+        if 'class_names' in l:
+            if not isinstance(l['class_names'], list) and not l['class_names'] is None:
+                raise Exception(f"class_names must be a list or null. Got {type(l['class_names'])}")
+            lane.class_names = l['class_names']
+
+        if 'confidences' in l:
+            lane.confidences = l['confidences']
+            processed_confidences = process_confidences(l['confidences'], lane.class_names)
+            lane.confidence = processed_confidences['confidence']
+            lane.metrics = {
+                **(lane.metrics if lane.metrics else {}), 
+                **(processed_confidences['metrics'] if processed_confidences['metrics'] else {})
+            }
+            lane.class_name = processed_confidences['class_name']
 
         if 'class_name' in l:
             lane.class_name = l['class_name']
