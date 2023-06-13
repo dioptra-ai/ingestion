a446e868672bdd47062de85c30788f78c1ce545e
Jerry Ng <jerryng500@hotmail.com>
2023-06-13 14:19:41 -0700

    Updated ingestion for completions. Removed prompt_responses

---
+++ helpers/completions.py
@@ -0,0 +1,27 @@
+from schemas.pgsql import models
+
+Completions = models.completion.Completion
+
+def process_completion_records(completions, pg_session, prediction=None, groundtruth=None):
+    for c in completions:
+        if 'id' in c:
+            completion = pg_session.query(Completions).filter(Completions.id == p['id']).first()
+        else:
+            completion = Completions(
+                organization_id=prediction.organization_id if prediction else groundtruth.organization_id,
+                prediction=prediction.id if prediction else None,
+                groundtruth=groundtruth.id if groundtruth else None,
+            )
+            pg_session.add(completion)
+        
+        if 'confidence' in c:
+            completion.confidence = c['confidence']
+        
+        if 'response' in c:
+            completion.response = c['response']
+
+        if 'metrics' in c:
+            completion.metrics = {
+                **(completion.metrics if completion.metrics else {}),
+                **(c['metrics'] if c.get('metrics') else {})
+            }
